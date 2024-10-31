1514b716f8aee6212266e1ea0fbd804a292908c1
LAB 2B-02 <halimahsyadiyah2202@gmail.com>
2024-10-31 16:33:25 +0700

    Add resources\views\components\landing file

---
+++ resources/views/components/landing/header/navbar/index.blade.php
@@ -0,0 +1,31 @@
+<div class="branding d-flex align-items-cente">
+
+    <div class="container position-relative d-flex align-items-center justify-content-between">
+        <a href="index.html" class="logo d-flex align-items-center">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/logo.png" alt=""> -->
+            <h1 class="sitename">STTI Cirebon</h1>
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+            <ul>
+                <li><a href="#hero" class="active">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#services">Prodi</a></li>
+                <li><a href="#faq">Informasi</a></li>
+                <li><a href="#team">Blog</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <!-- Mengganti link "Login" dengan button yang mengarah ke link pendaftaran -->
+                <li>
+                    <a href="https://pmb.stticirebon.ac.id">
+                        <button type="button" class="btn btn-primary">Pendaftaran</button>
+                    </a>
+                </li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+
+
+    </div>
+
+</div>
